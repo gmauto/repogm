88c932fcf981efab6101bbed62585989976ce49b
jason <donghongfei@aliyun.com>
2017-11-06 10:35:29 +0800

    20171106 更新了check_data.sh将database换成了变量

--- bin/check_data.sh
+++ bin/check_data.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #本脚本要求在 ori用户下执行
 #零件编号新增查询
+database="ori"
+conf_dir="/home/ori/general/bin/conf"
 function part_check() {
 spark-sql --master yarn --driver-memory 6g --executor-memory 6g --num-executors 20 --executor-cores 1 -e "
-use ori;
+use ${database};
 select DISTINCT *
 from
 (select t1.part_number as pn1,t2.part_num as pn2
@@ -29,11 +31,11 @@ where pn2 is null;
 #品牌车型车型 label_order check
 function car_order_check() {
 spark-sql --master yarn --driver-memory 6g --executor-memory 6g --num-executors 20 --executor-cores 1 -e "
-use ori;
+use ${database};
 select distinct makes,series,model
 from label_order
 " > mark_tmp
-cat /home/ori/general/bin/conf/CHE | awk -F "\t" '{print $1"\t"$2"\t"$3}' | sort -u >> mark_tmp
+cat ${conf_dir}/CHE | awk -F "\t" '{print $1"\t"$2"\t"$3}' | sort -u >> mark_tmp
 cat mark_tmp |grep -v '\t'|grep -v java | sort |uniq -u > mark_new
 }
 
@@ -41,10 +43,11 @@ cat mark_tmp |grep -v '\t'|grep -v java | sort |uniq -u > mark_new
 #doss 车系车型
 function car_doss_check() {
 spark-sql --master yarn --driver-memory 6g --executor-memory 6g --num-executors 20 --executor-cores 1 -e "
+use ${database};
 select distinct makes,series
-from ori.label_doss
+from label_doss
 " > mark_doss_tmp
-cat /home/ori/general/bin/conf/mark_doss |awk -F "\t" '{print $1"\t"$2}' >>mark_doss_tmp
+cat ${conf_dir}/mark_doss |awk -F "\t" '{print $1"\t"$2}' >>mark_doss_tmp
 cat mark_doss_tmp |grep -v '\t'|grep -v java | sort |uniq -u > mark_doss_new
 }
 
@@ -53,73 +56,73 @@ cat mark_doss_tmp |grep -v '\t'|grep -v java | sort |uniq -u > mark_doss_new
 function flow_dim(){
 #sexual
 spark-sql -e "
-use ori;
+use ${database};
 select t1.*
 from
 (select distinct trim(sexual) as sexual
-from flow_tmp) t1 left join (select distinct trim(sexual) as sexual from ori.sexual) t2
+from flow_tmp) t1 left join (select distinct trim(sexual) as sexual from sexual) t2
 on t1.sexual=t2.sexual
 where t2.sexual is null;
 " | grep -v '\t' | grep -v java >sexual_new
 
 #province
 spark-sql -e "
-use ori;
+use ${database};
 select t1.*
 from
 (select distinct trim(province) as province
-from flow_tmp) t1 left join (select distinct trim(province) as province from ori.province) t2
+from flow_tmp) t1 left join (select distinct trim(province) as province from province) t2
 on t1.province=t2.province
 where t2.province is null;
 " | grep -v '\t' | grep -v java >province_new
 
 #city
 spark-sql -e "
-use ori;
+use ${database};
 select t1.*
 from
 (select distinct trim(city) as city
-from flow_tmp) t1 left join (select distinct trim(city) as city from ori.city) t2
+from flow_tmp) t1 left join (select distinct trim(city) as city from city) t2
 on t1.city=t2.city
 where t2.city is null;
 " | grep -v '\t' | grep -v java >city_new
 
 #name
 spark-sql -e "
-use ori;
+use ${database};
 select t1.*
 from
 (select distinct trim(name) as name
-from flow_tmp) t1 left join (select distinct trim(name) as name from ori.name) t2
+from flow_tmp) t1 left join (select distinct trim(name) as name from name) t2
 on t1.name=t2.name
 where t2.name is null;
 " | grep -v '\t' | grep -v java >name_new
 
 #primary_classification
 spark-sql -e "
-use ori;
+use ${database};
 select t1.*
 from
 (select distinct trim(primary_classification) as primary_classification
-from flow_tmp) t1 left join (select distinct trim(primary_classification) as primary_classification from ori.primary_classification) t2
+from flow_tmp) t1 left join (select distinct trim(primary_classification) as primary_classification from primary_classification) t2
 on t1.primary_classification=t2.primary_classification
 where t2.primary_classification is null;
 " | grep -v '\t' | grep -v java >primary_classification_new
 
 #second_level_classification
 spark-sql -e "
-use ori;
+use ${database};
 select t1.*
 from
 (select distinct trim(second_level_classification) as second_level_classification
-from flow_tmp) t1 left join (select distinct trim(second_level_classification) as second_level_classification from ori.second_level_classification) t2
+from flow_tmp) t1 left join (select distinct trim(second_level_classification) as second_level_classification from second_level_classification) t2
 on t1.second_level_classification=t2.second_level_classification
 where t2.second_level_classification is null;
 " | grep -v '\t' | grep -v java >second_level_classification_new
 
 #distributor
 spark-sql -e "
-use ori;
+use ${database};
 set spark.sql.crossJoin.enabled = true;
 select  t1.chcode,t1.asccode,t1.asc
 from
